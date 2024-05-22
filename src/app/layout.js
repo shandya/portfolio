fdc58838f874540ee9fa1034fbae2fbfcc460d36
shandya-fas <113876129+shandya-fas@users.noreply.github.com>
2024-05-23 00:28:58 +0700

    fix: layout and meta info

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,15 @@
-import { Noto_Sans_Mono } from "next/font/google";
 import "@/app/globals.css";
+import { config } from '@fortawesome/fontawesome-svg-core';
+import '@fortawesome/fontawesome-svg-core/styles.css';
+import './fontawesome'; // This imports the configuration we made earlier
+import { Noto_Sans_Mono } from "next/font/google";
 
 const noto = Noto_Sans_Mono({ subsets: ["latin"] });
+config.autoAddCss = false; // Tell FontAwesome to not add its own CSS since we did it above
 
 export const metadata = {
   title: "Shandy Ardiansyah",
-  description: "Portfolio page",
+  description: "Portfolio Website",
 };
 
 export default function RootLayout({ children }) {
