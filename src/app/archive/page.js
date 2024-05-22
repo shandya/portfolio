161a04289ecc439093eab9ffc297eb0eefa7d6f6
shandya-fas <113876129+shandya-fas@users.noreply.github.com>
2024-05-23 00:42:41 +0700

    fix: disable dark mode. Update meta

--- src/app/archive/page.js
+++ src/app/archive/page.js
@@ -3,7 +3,6 @@
 
 import React, { useState } from 'react';
 import Link from 'next/link'
-import Head from 'next/head';
 import PortfolioInfo from '@/data/portfolio.json'
 
 import Gradient from "../components/Gradient";
@@ -27,109 +26,101 @@ export default function Archive() {
   };
 
   return (
-    <>
-      <Head>
-        <title>Archive | Shandy Ardiansyah</title>
-        <meta name="description" content="Archive page showcasing a diverse range of projects and creative works" />
-        <meta property="og:title" content="Archive | Shandy Ardiansyah" />
-        <meta property="og:description" content="Archive page showcasing a diverse range of projects and creative works" />
-      </Head>
-      <main className="flex min-h-screen flex-col items-center justify-between py-10 md:py-16 lg:py-24">
-        <section className="px-4 max-w-screen-xl relative z-10">
-          <header className="mb-10">
-            {/* <a href="/" className="text-xl mb-2 inline-block text-[#ccdbe0]">Return</a> */}
-            <h1 className="text-4xl font-bold text-[#31ecff] opacity-60">Archive</h1>
-          </header>
+    <main className="flex min-h-screen flex-col items-center justify-between py-10 md:py-16 lg:py-24">
+      <section className="px-4 max-w-screen-xl relative z-10">
+        <header className="mb-10">
+          {/* <a href="/" className="text-xl mb-2 inline-block text-[#ccdbe0]">Return</a> */}
+          <h1 className="text-4xl font-bold text-[#31ecff] opacity-60">Archive</h1>
+        </header>
 
-          <div className="">
-            <table className="-mx-4 hidden lg:block">
+        <div className="">
+          <table className="-mx-4 hidden lg:block">
+            <thead className="mb-10 sticky bg-[#31ecff] top-0 z-10 bg-opacity-30 text-[#ccdbe0]">
+              <tr className="text-left text-sm font-semibold">
+                <th className="p-4">Year</th>
+                <th className="p-4 w-1/4">Project</th>
+                <th className="p-4 w-1/6">Client</th>
+                <th className="p-4 w-1/6 whitespace-nowrap">Made At</th>
+                <th className="p-4 w-1/6">Technology</th>
+                <th className="p-4 whitespace-nowrap">External Link</th>
+              </tr>
+            </thead>
+            <tbody>
+              {portfolioData.map((item, index) => (
+                <tr key={index} className="border-b border-slate-300 border-opacity-30 lg:opacity-60 hover:opacity-100 ease-in transition-all duration-150 text-[#31ecff]">
+                  <td onClick={handleOpenModal(item.details)} className={`${item.details ? 'cursor-pointer' : ''} px-4 py-5 text-sm text-[#ccdbe0]`}>{item.year}</td>
+                  <td onClick={handleOpenModal(item.details)} className={`${item.details ? 'cursor-pointer' : ''} px-4 py-5 font-semibold leading-tight`}>{item.name}</td>
+                  <td onClick={handleOpenModal(item.details)} className={`${item.details ? 'cursor-pointer' : ''} px-4 py-5 leading-tight text-sm`}>{item.client}</td>
+                  <td onClick={handleOpenModal(item.details)} className={`${item.details ? 'cursor-pointer' : ''} px-4 py-5 leading-tight text-sm`}>{item.made_at}</td>
+                  <td onClick={handleOpenModal(item.details)} className={`${item.details ? 'cursor-pointer' : ''} px-4 py-5 flex flex-wrap gap-3`}>
+                    {item.tags.split(',').map((item, i) => (
+                      <span key={i} className="text-[#31ecff] py-1 px-3 bg-opacity-30 bg-[#31ecff] inline-block rounded-3xl text-[0.7em]">
+                        {item}
+                      </span>
+                    ))}
+                  </td>
+                  <td className="px-4 py-3">
+                    <a className="text-sm line-clamp-1" href={item.external_url} target="_blank">{item.external_url.replace('https://', '')}</a>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+
+          <div className="-mx-4">
+            <table className="lg:hidden w-full">
               <thead className="mb-10 sticky bg-[#31ecff] top-0 z-10 bg-opacity-30 text-[#ccdbe0]">
-                <tr className="text-left text-sm font-semibold">
+                <tr className="text-left text-sm font-semibold w-30">
                   <th className="p-4">Year</th>
-                  <th className="p-4 w-1/4">Project</th>
-                  <th className="p-4 w-1/6">Client</th>
-                  <th className="p-4 w-1/6 whitespace-nowrap">Made At</th>
-                  <th className="p-4 w-1/6">Technology</th>
-                  <th className="p-4 whitespace-nowrap">External Link</th>
+                  <th className="p-4">Project</th>
                 </tr>
               </thead>
               <tbody>
                 {portfolioData.map((item, index) => (
                   <tr key={index} className="border-b border-slate-300 border-opacity-30 lg:opacity-60 hover:opacity-100 ease-in transition-all duration-150 text-[#31ecff]">
-                    <td onClick={handleOpenModal(item.details)} className={`${item.details ? 'cursor-pointer' : ''} px-4 py-5 text-sm text-[#ccdbe0]`}>{item.year}</td>
-                    <td onClick={handleOpenModal(item.details)} className={`${item.details ? 'cursor-pointer' : ''} px-4 py-5 font-semibold leading-tight`}>{item.name}</td>
-                    <td onClick={handleOpenModal(item.details)} className={`${item.details ? 'cursor-pointer' : ''} px-4 py-5 leading-tight text-sm`}>{item.client}</td>
-                    <td onClick={handleOpenModal(item.details)} className={`${item.details ? 'cursor-pointer' : ''} px-4 py-5 leading-tight text-sm`}>{item.made_at}</td>
-                    <td onClick={handleOpenModal(item.details)} className={`${item.details ? 'cursor-pointer' : ''} px-4 py-5 flex flex-wrap gap-3`}>
-                      {item.tags.split(',').map((item, i) => (
-                        <span key={i} className="text-[#31ecff] py-1 px-3 bg-opacity-30 bg-[#31ecff] inline-block rounded-3xl text-[0.7em]">
-                          {item}
-                        </span>
-                      ))}
-                    </td>
-                    <td className="px-4 py-3">
-                      <a className="text-sm line-clamp-1" href={item.external_url} target="_blank">{item.external_url.replace('https://', '')}</a>
+                    <td className="px-4 py-5 text-sm text-[#ccdbe0] align-top">{item.year}</td>
+                    <td className="px-4 py-5 flex flex-col gap-4">
+                      {item.name && <h3 className="font-semibold leading-tight">{item.name}</h3>}
+                      {item.client && <p className="leading-tight text-sm">{item.client}</p>}
+                      {item.made_at && <p className="leading-tight text-sm">{item.made_at}</p>}
+                      <div className="flex flex-wrap gap-3">
+                        {item.tags.split(',').map((item, i) => (
+                          <span key={i} className="text-[#31ecff] py-1 px-3 bg-opacity-30 bg-[#31ecff] inline-block rounded-3xl text-[0.7em]">
+                            {item}
+                          </span>
+                        ))}
+                      </div>
+                      <div className="">
+                        {item.external_url &&
+                          <a className="text-[0.7em] flex gap-2 items-center underline underline-offset-4" href={item.external_url} target="_blank">
+                            <span>External url</span>
+                            <FontAwesomeIcon icon={faArrowRight} className="-rotate-45 h-3" />
+                          </a>
+                        }
+                      </div>
                     </td>
                   </tr>
                 ))}
               </tbody>
             </table>
-
-            <div className="-mx-4">
-              <table className="lg:hidden w-full">
-                <thead className="mb-10 sticky bg-[#31ecff] top-0 z-10 bg-opacity-30 text-[#ccdbe0]">
-                  <tr className="text-left text-sm font-semibold w-30">
-                    <th className="p-4">Year</th>
-                    <th className="p-4">Project</th>
-                  </tr>
-                </thead>
-                <tbody>
-                  {portfolioData.map((item, index) => (
-                    <tr key={index} className="border-b border-slate-300 border-opacity-30 lg:opacity-60 hover:opacity-100 ease-in transition-all duration-150 text-[#31ecff]">
-                      <td className="px-4 py-5 text-sm text-[#ccdbe0] align-top">{item.year}</td>
-                      <td className="px-4 py-5 flex flex-col gap-4">
-                        {item.name && <h3 className="font-semibold leading-tight">{item.name}</h3>}
-                        {item.client && <p className="leading-tight text-sm">{item.client}</p>}
-                        {item.made_at && <p className="leading-tight text-sm">{item.made_at}</p>}
-                        <div className="flex flex-wrap gap-3">
-                          {item.tags.split(',').map((item, i) => (
-                            <span key={i} className="text-[#31ecff] py-1 px-3 bg-opacity-30 bg-[#31ecff] inline-block rounded-3xl text-[0.7em]">
-                              {item}
-                            </span>
-                          ))}
-                        </div>
-                        <div className="">
-                          {item.external_url &&
-                            <a className="text-[0.7em] flex gap-2 items-center underline underline-offset-4" href={item.external_url} target="_blank">
-                              <span>External url</span>
-                              <FontAwesomeIcon icon={faArrowRight} className="-rotate-45 h-3" />
-                            </a>
-                          }
-                        </div>
-                      </td>
-                    </tr>
-                  ))}
-                </tbody>
-              </table>
-            </div>
           </div>
+        </div>
 
-          <footer className="py-10">
-            <Link href="/" className="mb-2 inline-block text-[#ccdbe0]">Back to home</Link>
-          </footer>
-        </section>
+        <footer className="py-10">
+          <Link href="/" className="mb-2 inline-block text-[#ccdbe0]">Back to home</Link>
+        </footer>
+      </section>
 
-        <Gradient />
+      <Gradient />
 
-        <Modal
-          isOpen={isModalOpen}
-          onClose={handleCloseModal}
-          title="Modal Title"
-          description="This is a description of the modal."
-          image="https://via.placeholder.com/400"
-        />
+      <Modal
+        isOpen={isModalOpen}
+        onClose={handleCloseModal}
+        title="Modal Title"
+        description="This is a description of the modal."
+        image="https://via.placeholder.com/400"
+      />
 
-      </main>
-    </>
+    </main>
   );
 }
--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,5 @@
 import Link from 'next/link'
-import Head from 'next/head';
-
+ 
 import WorkInfo from '@/data/work.json'
 import PortfolioInfo from '@/data/portfolio.json'
 
@@ -10,144 +9,141 @@ import Footer from "@/app/components/Footer";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowRight } from '@fortawesome/free-solid-svg-icons';
 
+export const metadata = {
+  title: 'Shandy Ardiansyah',
+  description: 'Portfolio website'
+}
+
 export default function Home() {
   const workData = WorkInfo.Work
   const portfolioData = PortfolioInfo.Portfolio
 
   return (
-    <>
-      <Head>
-        <title>Shandy Ardiansyah</title>
-        <meta name="description" content="Portfolio website" />
-        <meta property="og:title" content="Shandy Ardiansyah" />
-        <meta property="og:description" content="Portfolio website" />
-      </Head>
-      <main className="flex min-h-screen flex-col items-center justify-between lg:py-12">
-        <section className="px-4 w-full lg:fixed top-0 left-0 z-50">
-          <header className="max-w-screen-xl mx-auto relative">
-            <div className="w-full md:w-1/2 lg:absolute left-0 py-6 md:py-16 lg:py-24">
-              <h1 className="text-4xl font-bold text-[#31ecff] opacity-60 mb-2">Shandy Ardiansyah</h1>
-              <p className="text-xl inline-block text-[#ccdbe0]">Frontend Developer</p>
+    <main className="flex min-h-screen flex-col items-center justify-between lg:py-12">
+      <section className="px-4 w-full lg:fixed top-0 left-0 z-50">
+        <header className="max-w-screen-xl mx-auto relative">
+          <div className="w-full md:w-1/2 lg:absolute left-0 py-6 md:py-16 lg:py-24">
+            <h1 className="text-4xl font-bold text-[#31ecff] opacity-60 mb-2">Shandy Ardiansyah</h1>
+            <p className="text-xl inline-block text-[#ccdbe0]">Frontend Developer</p>
 
-              <nav className="mt-10">
-                <ul className="text-[#ccdbe0] flex lg:flex-col gap-4 flex-wrap text-sm lg:text-base">
-                  <li>
-                    <Link className="lg:opacity-60 hover:opacity-100 ease-in transition-all duration-150" href="#about">About</Link>
-                  </li>
-                  <li>
-                    <Link className="lg:opacity-60 hover:opacity-100 ease-in transition-all duration-150" href="#experience">Experience</Link>
-                  </li>
-                  <li>
-                    <Link className="lg:opacity-60 hover:opacity-100 ease-in transition-all duration-150" href="#projects">Projects</Link>
-                  </li>
-                  <li>
-                    <Link className="lg:opacity-60 hover:opacity-100 ease-in transition-all duration-150" href="/archive">Archive</Link>
-                  </li>
-                </ul>
-              </nav>
-            </div>
-          </header>
-        </section>
-
-        <section id="about" className="py-10 md:py-12 relative z-20 w-full">
-          <div className="max-w-screen-xl mx-auto">
-            <div className="w-full md:w-1/2 ml-auto px-4">
-              <h2 className="text-[#31ecff] opacity-60 text-lg mb-4 font-semibold">About</h2>
-
-              <p className="text-[#ccdbe0] text-sm mb-4">I'm a Front End Developer with ten years of experience building websites in the tech & creative advertising industries. I am passionate about creating intuitive web experiences using the latest technologies in the market.</p>
-              <p className="text-[#ccdbe0] text-sm">I have developed websites for numerous brands and campaigns in F&B, banking, technology, and education and
-  entertainment industries. I also picked up back end development here and there along my career in the industry, but hey
-  there are still a lot to learn!</p>          
-            </div>
+            <nav className="mt-10">
+              <ul className="text-[#ccdbe0] flex lg:flex-col gap-4 flex-wrap text-sm lg:text-base">
+                <li>
+                  <Link className="lg:opacity-60 hover:opacity-100 ease-in transition-all duration-150" href="#about">About</Link>
+                </li>
+                <li>
+                  <Link className="lg:opacity-60 hover:opacity-100 ease-in transition-all duration-150" href="#experience">Experience</Link>
+                </li>
+                <li>
+                  <Link className="lg:opacity-60 hover:opacity-100 ease-in transition-all duration-150" href="#projects">Projects</Link>
+                </li>
+                <li>
+                  <Link className="lg:opacity-60 hover:opacity-100 ease-in transition-all duration-150" href="/archive">Archive</Link>
+                </li>
+              </ul>
+            </nav>
           </div>
-        </section>
+        </header>
+      </section>
 
-        <section id="experience" className="py-10 md:py-12 relative z-20 w-full">
-          <div className="max-w-screen-xl mx-auto">
-            <div className="w-full md:w-1/2 ml-auto px-4">
-              <h2 className="text-[#31ecff] opacity-60 text-lg mb-4 font-semibold">Experience</h2>
+      <section id="about" className="py-10 md:py-12 relative z-20 w-full">
+        <div className="max-w-screen-xl mx-auto">
+          <div className="w-full md:w-1/2 ml-auto px-4">
+            <h2 className="text-[#31ecff] opacity-60 text-lg mb-4 font-semibold">About</h2>
 
-              <div className="-mx-4">
-                <table className="w-full">
-                  <tbody>
-                    {workData.map((item, index) => (
-                      <tr key={index} className="border-b border-slate-300 border-opacity-30 lg:opacity-60 hover:opacity-100 ease-in transition-all duration-150 text-[#31ecff]">
-                        <td className="px-4 py-5 text-sm text-[#ccdbe0] align-top whitespace-nowrap w-30">{item.time}</td>
-                        <td className="px-4 py-5 flex flex-col gap-4">
-                          {item.title && <h3 className="font-semibold leading-tight">{item.title}</h3>}
-                          {item.company_name && <p className="leading-tight text-sm">{item.company_name}</p>}
-                          {item.location && <p className="leading-tight text-sm">{item.location}</p>}
-                          {/* <div className="flex flex-wrap gap-3">
-                            {item.tags.split(',').map((item, i) => (
-                              <span key={i} className="text-[#31ecff] py-1 px-3 bg-opacity-30 bg-[#31ecff] inline-block rounded-3xl text-[0.7em]">
-                                {item}
-                              </span>
-                            ))}
-                          </div> */}
-                          {/* {item.external_url && <a className="text-sm line-clamp-1" href={item.external_url} target="_blank">{item.external_url.replace('https://', '')}</a>} */}
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-
-              <p className="mt-8 inline-block text-[#ccdbe0] text-sm">Request for full resume? <Link href="mailto:shandy.ardiansyah@gmail.com" className="text-[#31ecff]">Mail me.</Link></p>
-            </div>
+            <p className="text-[#ccdbe0] text-sm mb-4">I'm a Front End Developer with ten years of experience building websites in the tech & creative advertising industries. I am passionate about creating intuitive web experiences using the latest technologies in the market.</p>
+            <p className="text-[#ccdbe0] text-sm">I have developed websites for numerous brands and campaigns in F&B, banking, technology, and education and
+entertainment industries. I also picked up back end development here and there along my career in the industry, but hey
+there are still a lot to learn!</p>          
           </div>
-        </section>
+        </div>
+      </section>
 
-        <section id="projects" className="py-10 md:py-12 relative z-20 w-full">
-          <div className="max-w-screen-xl mx-auto">
-            <div className="w-full md:w-1/2 ml-auto px-4">
-              <h2 className="text-[#31ecff] opacity-60 text-lg mb-4 font-semibold">Projects</h2>
+      <section id="experience" className="py-10 md:py-12 relative z-20 w-full">
+        <div className="max-w-screen-xl mx-auto">
+          <div className="w-full md:w-1/2 ml-auto px-4">
+            <h2 className="text-[#31ecff] opacity-60 text-lg mb-4 font-semibold">Experience</h2>
 
-              <div className="-mx-4">
-                <table className="w-full">
-                  {/* <thead className="mb-10 sticky bg-[#31ecff] top-0 z-10 bg-opacity-30 text-[#ccdbe0]">
-                    <tr className="text-left text-sm font-semibold">
-                      <th className="p-4">Year</th>
-                      <th className="p-4">Project</th>
+            <div className="-mx-4">
+              <table className="w-full">
+                <tbody>
+                  {workData.map((item, index) => (
+                    <tr key={index} className="border-b border-slate-300 border-opacity-30 lg:opacity-60 hover:opacity-100 ease-in transition-all duration-150 text-[#31ecff]">
+                      <td className="px-4 py-5 text-sm text-[#ccdbe0] align-top whitespace-nowrap w-30">{item.time}</td>
+                      <td className="px-4 py-5 flex flex-col gap-4">
+                        {item.title && <h3 className="font-semibold leading-tight">{item.title}</h3>}
+                        {item.company_name && <p className="leading-tight text-sm">{item.company_name}</p>}
+                        {item.location && <p className="leading-tight text-sm">{item.location}</p>}
+                        {/* <div className="flex flex-wrap gap-3">
+                          {item.tags.split(',').map((item, i) => (
+                            <span key={i} className="text-[#31ecff] py-1 px-3 bg-opacity-30 bg-[#31ecff] inline-block rounded-3xl text-[0.7em]">
+                              {item}
+                            </span>
+                          ))}
+                        </div> */}
+                        {/* {item.external_url && <a className="text-sm line-clamp-1" href={item.external_url} target="_blank">{item.external_url.replace('https://', '')}</a>} */}
+                      </td>
                     </tr>
-                  </thead> */}
-                  <tbody>
-                    {portfolioData.filter((item) => (item.highlight)).map((item, index) => (
-                      <tr key={index} className="border-b border-slate-300 border-opacity-30 lg:opacity-60 hover:opacity-100 ease-in transition-all duration-150 text-[#31ecff]">
-                        <td className="px-4 py-5 text-sm text-[#ccdbe0] align-top w-32">{item.year}</td>
-                        <td className="px-4 py-5 flex flex-col gap-4">
-                          {item.name && <h3 className="font-semibold leading-tight">{item.name}</h3>}
-                          {/* {item.client && <p className="leading-tight text-sm">{item.client}</p>}
-                          {item.made_at && <p className="leading-tight text-sm">{item.made_at}</p>} */}
-                          <div className="flex flex-wrap gap-3">
-                            {item.tags.split(',').map((item, i) => (
-                              <span key={i} className="text-[#31ecff] py-1 px-3 bg-opacity-30 bg-[#31ecff] inline-block rounded-3xl text-[0.7em]">
-                                {item}
-                              </span>
-                            ))}
-                          </div>
-                          <div className="">
-                            {item.external_url &&
-                              <Link className="text-[0.7em] flex gap-2 items-center underline underline-offset-4" href={item.external_url} target="_blank">
-                                <span>External url</span>
-                                <FontAwesomeIcon icon={faArrowRight} className="-rotate-45 h-3" />
-                              </Link>
-                            }
-                          </div>
-                        </td>
-                      </tr>
-                    ))}
-                  </tbody>
-                </table>
-              </div>
-              <Link href="/archive" className="mt-8 inline-block text-[#ccdbe0] text-sm">View Full Archive</Link>
+                  ))}
+                </tbody>
+              </table>
             </div>
+
+            <p className="mt-8 inline-block text-[#ccdbe0] text-sm">Request for full resume? <Link href="mailto:shandy.ardiansyah@gmail.com" className="text-[#31ecff]">Mail me.</Link></p>
           </div>
-        </section>
+        </div>
+      </section>
+
+      <section id="projects" className="py-10 md:py-12 relative z-20 w-full">
+        <div className="max-w-screen-xl mx-auto">
+          <div className="w-full md:w-1/2 ml-auto px-4">
+            <h2 className="text-[#31ecff] opacity-60 text-lg mb-4 font-semibold">Projects</h2>
+
+            <div className="-mx-4">
+              <table className="w-full">
+                {/* <thead className="mb-10 sticky bg-[#31ecff] top-0 z-10 bg-opacity-30 text-[#ccdbe0]">
+                  <tr className="text-left text-sm font-semibold">
+                    <th className="p-4">Year</th>
+                    <th className="p-4">Project</th>
+                  </tr>
+                </thead> */}
+                <tbody>
+                  {portfolioData.filter((item) => (item.highlight)).map((item, index) => (
+                    <tr key={index} className="border-b border-slate-300 border-opacity-30 lg:opacity-60 hover:opacity-100 ease-in transition-all duration-150 text-[#31ecff]">
+                      <td className="px-4 py-5 text-sm text-[#ccdbe0] align-top w-32">{item.year}</td>
+                      <td className="px-4 py-5 flex flex-col gap-4">
+                        {item.name && <h3 className="font-semibold leading-tight">{item.name}</h3>}
+                        {/* {item.client && <p className="leading-tight text-sm">{item.client}</p>}
+                        {item.made_at && <p className="leading-tight text-sm">{item.made_at}</p>} */}
+                        <div className="flex flex-wrap gap-3">
+                          {item.tags.split(',').map((item, i) => (
+                            <span key={i} className="text-[#31ecff] py-1 px-3 bg-opacity-30 bg-[#31ecff] inline-block rounded-3xl text-[0.7em]">
+                              {item}
+                            </span>
+                          ))}
+                        </div>
+                        <div className="">
+                          {item.external_url &&
+                            <Link className="text-[0.7em] flex gap-2 items-center underline underline-offset-4" href={item.external_url} target="_blank">
+                              <span>External url</span>
+                              <FontAwesomeIcon icon={faArrowRight} className="-rotate-45 h-3" />
+                            </Link>
+                          }
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </table>
+            </div>
+            <Link href="/archive" className="mt-8 inline-block text-[#ccdbe0] text-sm">View Full Archive</Link>
+          </div>
+        </div>
+      </section>
 
 
-        <Footer />
-        <Gradient />
-      </main>
-    </>
+      <Footer />
+      <Gradient />
+    </main>
   );
 }
